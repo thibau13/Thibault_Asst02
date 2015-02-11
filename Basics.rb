9ef1907071a13ff9914ab299930b3c22fc9b85a2
thibau13 <thibau13@unlv.nevada.edu>
2015-02-11 01:32:41 -0800

    Create Basics.rb

---
+++ Basics.rb
@@ -0,0 +1,13 @@
+#Kyle Thibault
+#Assignment 02 - basics
+#Program prompts you for a name and prints the name with a greeting
+#Input - A name
+#Output - personalized greeting
+
+name = ""
+
+print "What is your name?"
+name = gets.chomp
+
+
+puts "Hello, #{name}"
